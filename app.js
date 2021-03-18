e16bf8843892e99a26283cc2b0c54b519c42dd82
rigobertoalag <a20150338@utem.edu.mx>
2021-03-18 12:37:31 -0600

    test commit 6

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const msyql = require('mysql')
 const myconn = require('express-myconnection')
+const routes = require('./routes')
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -11,10 +12,17 @@ const dbOptions = {
     database: 'freedbtech_apitest'
 }
 
+//Middleware
+app.use(myconn(msyql, dbOptions, 'single'))
+app.use(express.json())
+app.use(cors())
+
 app.get('/', function (req, res) {
-    res.send('Prueba de imports y dboptions');
+    res.send('Prueba rutas');
     console.log('desde el console')
 });
+
+app.use('/api', routes)
 
 app.listen(port, function () {
     console.log(`Example app listening on port !`);
---
+++ routes.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const routes = express.Router()
+
+/** GET METHODS*/
+
+//GET Todos los ahorros
+routes.get('/ahorro', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('SELECT * FROM ahorro', (err, rows)=>{
+            if(err) return res.send(err)
+            res.json(rows)
+        })
+    })
+})
+
+//GET Ahorro by ID
+routes.get('/ahorro/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('SELECT * FROM books WHERE id = ?',[req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+            res.json(rows)
+        })
+    })
+})
+
+/** POST METHODS*/
+
+//POST nuevo ahrro 
+routes.post('/ahorro', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('INSERT INTO ahorro SET ?', [req.body],(err, rows)=>{
+            if(err) return res.send(err)
+            res.send('Registro guardado')
+        })
+    })
+})
+
+/** GET METHODS TESTING*/
+
+//GET Todos los libros
+routes.get('/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('SELECT * FROM books', (err, rows)=>{
+            if(err) return res.send(err)
+            res.json(rows)
+        })
+    })
+})
+
+//GET Libro by ID
+routes.get('/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('SELECT * FROM books WHERE id = ?',[req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+            res.json(rows)
+        })
+    })
+})
+
+/** POST METHODS TESTING*/
+
+//POST nuevo libro 
+routes.post('/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('INSERT INTO books SET ?', [req.body],(err, rows)=>{
+            if(err) return res.send(err)
+            res.send('Registro guardado')
+        })
+    })
+})
+
+module.exports = routes
